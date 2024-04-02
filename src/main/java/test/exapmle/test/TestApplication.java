b39f68dce56a6c9c5a4d505f7db6ce3bf70d0a54
bharath-vcti <150214376+bharath-vcti@users.noreply.github.com>
2024-04-02 11:01:41 +0530

    Add files via upload

---
+++ src/main/java/test/exapmle/test/TestApplication.java
@@ -0,0 +1,21 @@
+package test.exapmle.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String applicationWorking() {
+		return "Application is up and running...";
+	}
+}
